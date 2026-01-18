b444e0d9e84b89582d1930704836872e442827be
Anjali Pal <11anjalipal@gmail.com>
2026-01-19 05:02:14 +0530

    Js version (DOM)

--- script.js
+++ script.js
@@ -1,37 +1,59 @@
-var choices = ["rock" , "paper", "scissors"]
-
+let choices = ["rock", "paper", "scissors"]
 function computer() {
     var n = Math.floor((Math.random() * 3))
     return choices[n];
 }
 
-function game() {
-    var userChoice = prompt("Enter 1-Rock, 2-Paper, 3-Scissors")
-    if (userChoice == 1) {
+let userChoice = ""
+let uscore = 0
+let total = 0
+    let r = document.querySelector(".R")
+    r.addEventListener("click", setVal)
+    function setVal() {
         userChoice = "rock"
+        game()
     }
-    else if(userChoice == 2) {
+    let p = document.querySelector(".P")
+    p.addEventListener("click", setVal2)
+    function setVal2() {
         userChoice = "paper"
+        game()
     }
-    else if (userChoice == 3) {
+    let s = document.querySelector(".S")
+    s.addEventListener("click", setVal3)
+    function setVal3() {
         userChoice = "scissors"
+        game()
     }
-    else {
-        console.log("Invalid choice")
-        return
+    let set = document.querySelector(".Set")
+    set.addEventListener("click", reset)
+    function reset() {
+        uscore = 0
+        total = 0
+        let res = document.querySelector(".output")
+        res.innerText = "New Game"
     }
 
+
+function game() {
+    total++
     var computerChoice = computer()
     if (userChoice == computerChoice) {
-        console.log("It's a tie")
+        let res = document.querySelector(".output")
+        uscore++
+        total++;
+        res.innerText = `It's a tie, Your Score : ${uscore} , Compute Score : ${total - uscore}`
     }
     else if  ( (userChoice === "rock" && computerChoice === "scissors") ||
     (userChoice === "paper" && computerChoice === "rock") ||
     (userChoice === "scissors" && computerChoice === "paper") ) {
-        console.log(`You win, computer chose ${computerChoice}`)
+        let res = document.querySelector(".output")
+        uscore++
+        res.innerText = `You win, computer chose ${computerChoice}, Your Score : ${uscore} , Compute Score : ${total - uscore}`
+
     }
     else {
-        console.log(`You lose, computer chose ${computerChoice}`)
+        let res = document.querySelector(".output")
+        res.innerText = `You lose, computer chose ${computerChoice}, Your Score : ${uscore} , Compute Score : ${total - uscore}`
     }
 }
-game()
